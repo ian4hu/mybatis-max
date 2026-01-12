208e609393ca983a6c36aa2561171e4e633d04a1
Ian Hu <hu2008yinxiang@163.com>
2026-01-12 09:11:04 +0000

    codestyle

--- src/main/kotlin/com/github/ian4hu/mybatis/max/expr/Alias.kt
+++ src/main/kotlin/com/github/ian4hu/mybatis/max/expr/Alias.kt
@@ -16,8 +16,8 @@
 package com.github.ian4hu.mybatis.max.expr
 
 import com.baomidou.mybatisplus.core.conditions.AbstractWrapper
-import com.github.ian4hu.mybatis.max.Renderable
 import com.github.ian4hu.mybatis.max.Expr
+import com.github.ian4hu.mybatis.max.Renderable
 
 /**
  * Represents a SQL alias wrapper that renders an expression with an optional alias name.
